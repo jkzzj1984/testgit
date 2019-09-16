f8a14285951ff6e0d7ed5f2fa6195ad735308618
zhangzhijun <zjzhang@railwaydetect.com>
2019-09-16 17:28:57 +0800

    项目文件

---
+++ myweb/Controllers/aController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myweb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class aController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string A,string B)
+        {
+            return "BBBB," + A + "," + B;
+        }
+    }
+}
